36ae0af893052d5f7bf6474d7364621cda74e11d
Nada ali <elghoulnada17@gmail.com>
2026-09-05 16:57:24 +0300

    main serverurl

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { UserProvider } from "./context/UserContext";
 import { ProductProvider } from "./context/ProductContext";
 import { CartProvider } from "./context/CartContext";
 
-import.meta.env.VITE_SERVER_URL;
+import.meta.env.SERVER_URL;
 
 export const categories = [
   "smartphone",
